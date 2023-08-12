b5380b1fb608bbe2bbe802078c3533aa8095e6f3
조가람 <latack789@naver.com>
2023-08-12 09:57:46 +0900

    test

---
+++ garam/1181.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+temp = 0
+n = int(input())
+word_list = []
+for i in range(n):
+    word_list.append(input().strip())
+    # print(word_list)
+set = set(word_list)
+lst = list(set)
+lst.sort()
+
+for i in lst:
+    print(i)
