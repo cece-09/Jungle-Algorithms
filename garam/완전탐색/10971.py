8850e47fe433775e938a876bc7729ffe63103060
조가람 <latack789@naver.com>
2023-08-13 20:15:18 +0900

    외판원 문제

--- garam/완전탐색/10971.py
+++ garam/완전탐색/10971.py
@@ -1,6 +1,5 @@
 import sys
 import itertools
-
 input = sys.stdin.readline
 
 n = int(input())
@@ -8,14 +7,10 @@ W_arr = [list(map(int, input().rstrip().split())) for _ in range(n)]
 # W_arr = []
 # for i in range(n):
 #     W_arr.append(list(map(int, input().split())))
-
 a = [i for i in range(n)]
 # a = [0,1,2,3]
 total_case = list(itertools.permutations(a, n))
-
 # W_arr[i][j]의 조합을 만들어 주기 위함
-
-
 def check_case(W_arr, case):
     case = case + (case[0],) # 원래 자리로 돌아가야 하므로 순회니까~ ,위치 때문에 헷갈림
     cost = 0
@@ -25,7 +20,6 @@ def check_case(W_arr, case):
         cost += W_arr[case[i]][case[i + 1]]
 
     return cost
-
 
 answer = int(10e9)
 for case in total_case:
