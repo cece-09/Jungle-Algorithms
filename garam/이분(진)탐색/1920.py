ffd87641cfc9d5e20ac10dd2445be88a0479a4c6
조가람 <latack789@naver.com>
2023-08-14 23:19:11 +0900

    이진 탐색 여러가지 방법

---
+++ garam/이분(진)탐색/1920.py
@@ -0,0 +1,60 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+m = int(input())
+target_arr = list(map(int, input().split()))
+
+
+def binary_search(target_num, data):
+    start = 0
+    end = n - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+        if target_num == data[mid]:
+            return True
+        elif target_num < data[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return False
+
+
+for num in target_arr:
+    if binary_search(num, arr):
+        print(1)
+    else:
+        print(0)
+
+
+
+# 시간초과 난 답
+
+# n = map(int, input().split())
+# arr = list(map(int, input().split()))
+# m = map(int, input().split())
+# brr = list(map(int, input().split()))
+# crr = [0 for i in range(len(arr))]  # n으로 하니까 안되네;;
+# arr.sort()
+
+# for i in range(len(brr)):
+#     for j in range(len(brr)):
+#         if brr[i] == arr[j]:
+#             crr[i] = 1
+# for num in crr:
+#     print(num)
+
+# 시간초과는 아니지만 이진탐색이 아닌 경우
+# n = map(int, input().split())
+# arr = set(map(int, input().split()))
+# m = map(int, input().split())
+# brr = list(map(int, input().split()))
+# crr = [0 for i in range(len(arr))]  # n으로 하니까 안되네;;
+
+# for num in brr:
+#     if num in arr:
+#         print(1)
+#     else:
+#         print(0)
