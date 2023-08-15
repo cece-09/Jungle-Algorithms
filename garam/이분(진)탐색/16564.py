c84983dbe8d97390a64ddfeeb14de1224c55b7be
조가람 <latack789@naver.com>
2023-08-15 19:36:27 +0900

    히오스

--- garam/이분(진)탐색/16564.py
+++ garam/이분(진)탐색/16564.py
@@ -1,11 +1,24 @@
 import sys
 
 input = sys.stdin.readline
-n, k = map(int, input().split())
-x = []
-for _ in range(n):
-    x.append(int(input()))
-# x = list(map(int, input().split()) for _ in range(n)) # 이거 안되는데 어떻게 위 코드를 줄일 수 있지?
 
-print(n, k)
-print(x)
+n, k = map(int, input().split())
+x_arr = list(int(input()) for _ in range(n))
+
+# start와 end값을 무엇으로 할지 결정하는게 가장 어렵다. 내가 무엇을 구하고 싶은지 명확하게 하는게 힘들어
+start = min(x_arr)
+end = start + k
+ans = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    temp = 0
+    for x in x_arr:
+        if x <= mid:
+            temp += mid - x
+    if temp <= k:
+        start = mid + 1
+        ans = max(mid, ans)
+    else:
+        end = mid - 1
+print(ans)
--- garam/이분(진)탐색/2805.py
+++ garam/이분(진)탐색/2805.py
@@ -14,16 +14,12 @@ while start <= end:
     for tree in trees:
         if tree > mid:
             total += tree - mid
-    print(f'total =  {total}')
 
     if total >= m:
         start = mid + 1
-        print(f'start = {start}')
-        print(end)
+        
     else:
         end = mid - 1
-        print(start)
-        print(end)
-
+        
 print(end)
 
