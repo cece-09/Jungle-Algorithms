e4b6d96c35367c8b44222067f815ecd8b970236e
조가람 <latack789@naver.com>
2023-08-15 15:55:26 +0900

    두 용액 투포인터

--- garam/이분(진)탐색/2110while문 실패.py
+++ garam/이분(진)탐색/2110while문 실패.py
@@ -1,37 +1,35 @@
 import sys
+
 input = sys.stdin.readline
 # N = 집의 개수, C = 공유기 개수
-N, C = map(int, input().split()) 
+N, C = map(int, input().split())
 coor = []
 for _ in range(N):
     coor.append(int(input()))
-    
+
 coor.sort()
 
 def binary_search(coor, start, end):
-    mid = (start+end) //2
-    current_coor = coor[0]
-    cnt = 0
-    
-    while start < end:
-        for i in range(len(coor)):
+    while start <= end:
+        mid = (start + end) // 2
+        current_coor = coor[0]
+        cnt = 0
+        
+        for i in range(1, len(coor)):
             if coor[i] >= current_coor + mid:
                 cnt += 1
                 current_coor = coor[i]
+
         if cnt >= C:
+            global ans
             ans = mid
-            start = mid+1
+            start = mid + 1
         else:
             end = mid - 1
-            
-                
-            
-    return ans
 
-start = 1 
+start = 1
 end = coor[-1] - coor[0]
 ans = 0
-
 
 binary_search(coor, start, end)
 print(ans)
---
+++ garam/이분(진)탐색/2470.py
@@ -0,0 +1,31 @@
+# 두 용액 투포인터 활용
+import sys
+import itertools
+
+# from itertools import permutations
+
+input = sys.stdin.readline
+
+n = int(input())
+arr = sorted(list(map(int, input().split())))
+
+start = 0
+end = n - 1
+max = 1e9
+ans_1 = 0
+ans_2 = 0
+
+while start < end:
+    temp = arr[start] + arr[end]
+    abs_temp = abs(temp)
+    if max > abs_temp:
+        max = abs_temp
+        ans_1 = arr[start]
+        ans_2 = arr[end]
+# 이 부분이 헷갈리는데 0보다 크면 양수쪽을 줄여야 함
+    if temp > 0:
+        end -= 1
+    else:
+        start += 1
+        
+print(ans_1, ans_2)
