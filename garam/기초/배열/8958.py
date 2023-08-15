d4cec64ec331365dad9558dfccb8b3da647f2ba7
조가람 <latack789@naver.com>
2023-08-15 23:34:58 +0900

    헷갈리는 배열 다시보기

--- garam/기초/배열/8958.py
+++ garam/기초/배열/8958.py
@@ -1,4 +1,14 @@
 n = int(input())
 # arr = [list(map(str, input().split())) for i in range(n)]
-arr = [input()for i in range(n)]
+arr = [input() for i in range(n)]
 
+for a in arr:
+    total = 0
+    score = 0
+    for i in range(len(a)):
+        if a[i] == "O":
+            score += 1
+            total += score
+        else:
+            score = 0
+    print(total)
